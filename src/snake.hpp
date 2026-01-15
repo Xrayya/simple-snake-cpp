39634bd9a3002a02e8c919fe6b1c153958865a96
Xrayya <nur.alkhiyar@gmail.com>
2026-01-16 06:47:36 +0700

    feat(snake): make snake bidirectonal iterable

--- src/snake.hpp
+++ src/snake.hpp
@@ -41,15 +41,19 @@ public:
     tail = head->backNode.get();
   }
 
+  void move(Direction direction) override;
+  int eat(IFood &food);
+
   class Iterator {
   public:
-    using iterator_category = std::forward_iterator_tag;
+    using iterator_category = std::bidirectional_iterator_tag;
     using value_type = SnakeNode;
     using difference_type = std::ptrdiff_t;
     using pointer = SnakeNode *;
     using reference = SnakeNode &;
 
-    explicit Iterator(SnakeNode *node) : current(node) {}
+    explicit Iterator(SnakeNode *node, SnakeNode *tail)
+        : current(node), tail(tail) {}
 
     reference operator*() const { return *current; }
     pointer operator->() const { return current; }
@@ -57,6 +61,27 @@ public:
     Iterator &operator++() {
       current = current->backNode.get();
       return *this;
+    }
+
+    Iterator operator++(int) {
+      auto tmp = *this;
+      ++(*this);
+      return tmp;
+    }
+
+    Iterator &operator--() {
+      if (current == nullptr) {
+        current = tail;
+      } else {
+        current = current->frontNode;
+      }
+      return *this;
+    }
+
+    Iterator operator--(int) {
+      auto tmp = *this;
+      --(*this);
+      return tmp;
     }
 
     bool operator==(const Iterator &other) const {
@@ -69,13 +94,16 @@ public:
 
   private:
     SnakeNode *current;
+    SnakeNode *tail;
   };
 
-  Iterator begin() { return Iterator(head.get()); }
-  Iterator end() { return Iterator(tail->backNode.get()); }
+  Iterator begin() { return Iterator(head.get(), tail); }
+  Iterator end() { return Iterator(tail->backNode.get(), tail); }
 
-  void move(Direction direction) override;
-  int eat(IFood &food);
+  using ReverseIterator = std::reverse_iterator<Iterator>;
+
+  ReverseIterator rbegin() { return ReverseIterator(end()); }
+  ReverseIterator rend() { return ReverseIterator(begin()); }
 
 private:
   std::unique_ptr<SnakeNode> head;
