a78d76843a32a1c324a8db286d45963c7d5b8c30
Xrayya <nur.alkhiyar@gmail.com>
2026-01-16 10:36:21 +0700

    feat(food): add movable food and setup update and get score method

--- src/food/food.hpp
+++ src/food/food.hpp
@@ -1,10 +1,17 @@
 #pragma once
 
+#include "../core/movable.hpp"
 #include "../core/position.hpp"
 
 class IFood {
 public:
   virtual ~IFood() = default;
   virtual const Position &position() const = 0;
-  virtual int scoreAddition() = 0;
+  virtual void update() = 0;
+  virtual int getAdditionScore();
+
+private:
+  int additionScore = 0;
 };
+
+class IMovableFood : public IFood, IMovable {};
