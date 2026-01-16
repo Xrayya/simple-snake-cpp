07e453cbce0d12213a81108cd3c984c6071034c6
Xrayya <nur.alkhiyar@gmail.com>
2026-01-16 10:37:49 +0700

    feat(renderere): implement render function

--- src/renderer.cpp
+++ src/renderer.cpp
@@ -1,3 +1,12 @@
 #include "renderer.hpp"
+#include <iostream>
 
 Renderer::Renderer(const Game &game) : game(game) {}
+
+void Renderer::render() {
+  for (auto line : game.field) {
+    for (auto c : line) {
+      std::cout << c;
+    }
+  }
+}
