45ff4c71214c8cfacc44c5c43d9f58b1cc91f85c
Xrayya <nur.alkhiyar@gmail.com>
2026-01-16 14:35:55 +0700

    feat(position): add static method for generating random position

--- src/core/fundamentals/position.cpp
+++ src/core/fundamentals/position.cpp
@@ -1,5 +1,12 @@
 #include "position.hpp"
+#include "core/utils/utils.hpp"
+
+Position::Position(int x, int y) : x(x), y(y) {}
 
 bool Position::operator==(const Position &other) const {
   return x == other.x && y == other.y;
+}
+
+Position Position::random(int xMax, int yMax) {
+  return Position(generateRandomNum(0, xMax), generateRandomNum(0, yMax));
 }
--- src/core/fundamentals/position.hpp
+++ src/core/fundamentals/position.hpp
@@ -1,7 +1,10 @@
 #pragma once
 
-struct Position {
+class Position {
+public:
+  Position(int x, int y);
   int x, y;
 
   bool operator==(const Position &other) const;
+  static Position random(int xMax, int yMax);
 };
