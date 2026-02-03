479e65a564230d2cb2ca6bd226bb3f0cdccd91d4
Xrayya <nur.alkhiyar@gmail.com>
2026-02-03 17:31:11 +0700

    feat(tui renderer): merge icon into tui_renderer.cpp

--- src/game_core/renderer/tui_renderer.cpp
+++ src/game_core/renderer/tui_renderer.cpp
@@ -1,9 +1,24 @@
 #include "game_core/renderer/tui_renderer.hpp"
-#include "game_core/renderer/icon.hpp"
 #include <iostream>
 #include <ostream>
 #include <string>
 #include <vector>
+
+struct Icon {
+  struct Snake {
+    static const char Head = '@';
+    static const char Body = 'o';
+  };
+  struct Boundary {
+    static constexpr std::string TopLeftCorner = "╭";
+    static constexpr std::string TopRightCorner = "╮";
+    static constexpr std::string BonttomLeftCorner = "╰";
+    static constexpr std::string BottomRightCorner = "╯";
+    static constexpr std::string Horizontal = "─";
+    static constexpr std::string Vertical = "│";
+  };
+  static const char Food = '$';
+};
 
 TUIRenderer::TUIRenderer(std::shared_ptr<Game> game)
     : IRenderer(std::move(game)),
